03feb1061dbd7acf0d5711adc4fb3036838dfb88
Kai Xiang <kaixiang.xu@mail.utoronto.ca>
2023-09-11 12:03:14 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,6 +5,13 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
+        int i = 1;
+
+        while (i < 100){
+
+            i = doFizzBuzz(i);
+        }
+        /*
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
@@ -30,5 +37,31 @@ class FizzBuzz {
 
             }
         }
+         */
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 }
