91d74c2f7d926503725517e7f5eba7687941e9c7
Kai Xiang <kaixiang.xu@mail.utoronto.ca>
2023-09-11 13:34:11 -0400

    count number of multiples of 3 and 5 within 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i <1000; i++) {
+            if ((i%3 == 0) || (i%5 == 0)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
