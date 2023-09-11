04cb9b7f2250fa3eb7fa54a53a1e5045c859a24e
Kai Xiang <kaixiang.xu@mail.utoronto.ca>
2023-09-11 13:34:58 -0400

    count how many steps it takes to reduce 100 to 0

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int num = 100;
+        while (num >0){
+            if (num%2 != 0){
+                int newNum = num - 1;
+                num = newNum;
+            } else {
+                int newNum = num/2;
+                num = newNum;
+            }
+            count++;
+        }
+
+        System.out.println(count);
+    }
 }
